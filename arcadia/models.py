3545758793dbcf7e194007d6908871c71c70c9ab
yashvik <39464729+waverune@users.noreply.github.com>
2024-04-16 01:23:43 +0530

    ingest auction & repay events

--- arcadia/models.py
+++ arcadia/models.py
@@ -1,7 +1,6 @@
 from django.db import models
 from core.models import Transaction
 
-# Borrow (index_topic_1 address account, index_topic_2 address by, address to, uint256 amount, uint256 fee, index_topic_3 bytes3 referrer)
 class Borrow(Transaction):
     pool_address = models.TextField(null=False)
     account = models.TextField(null=False)
@@ -14,4 +13,25 @@ class Borrow(Transaction):
     def __str__(self):
         return f"{self.account=} {self.by=} {self.to=} {self.amount=} {self.fee=} {self.referrer=}"
 
+class AuctionStarted(Transaction):
+    pool_address = models.TextField(null=False)
+    account = models.TextField(null=False)
+    creditor = models.TextField(null=False)
+    open_debt = models.TextField(null=False)
 
+class AuctionFinished(Transaction):
+    pool_address = models.TextField(null=False)
+    account = models.TextField(null=False)
+    creditor = models.TextField(null=False)
+    start_debt = models.TextField(null=False)
+    initiation_reward = models.TextField(null=False)
+    termination_reward = models.TextField(null=False)
+    penalty = models.TextField(null=False)
+    bad_debt = models.TextField(null=False)
+    surplus = models.TextField(null=False)
+
+class Repay(Transaction):
+    pool_address = models.TextField(null=False)
+    account = models.TextField(null=False)
+    from_address = models.TextField(null=False)
+    amount = models.TextField(null=False)
