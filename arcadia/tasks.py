1084f3f36c7d35a200fbb0b130eed0e2ff3c493f
rik61072@gmail.com <rik61072@gmail.com>
2024-05-14 14:19:47 +0530

    feat: task_web3py_logs

--- arcadia/tasks.py
+++ arcadia/tasks.py
@@ -11,16 +11,16 @@ from django.db.models import Max
 from django.db.models import Sum, F, FloatField
 from django.db.models.functions import Cast
 
+# Hex cleaner function
+def hex_cleaner(param):
+    if isinstance(param, bytes):
+        return "0x" + str(param.hex()).lower()
+    elif isinstance(param, str):
+        return param.lower()
+
 # Arcadia events ingestion tasks:
-# 1. Borrow(address indexed account, address indexed by, address to, uint256 amount, uint256 fee, bytes3 indexed referrer);
-# 2. AuctionStarted(address indexed account, address indexed creditor, uint128 openDebt);
-# 3. AuctionFinished(address indexed account, address indexed creditor, uint256 startDebt, uint256 initiationReward, uint256 terminationReward, uint256 penalty, uint256 badDebt, uint256 surplus);
-# 4. Repay(address indexed account, address indexed from, uint256 amount);
-
-### Labels: are used so that the task knows where the parquet files are stored.
-
 @shared_task(name="task__arcadia__borrow_events")
-def task__arcadia__borrow(label: str, pool_address:str):
+def task__arcadia__borrow(label: str, pool_address: str):
 
     metadata = CryoLogsMetadata.objects.get(label=label)
     files, after_ingestion = parquet_files_to_process(metadata.ingested, label)
@@ -33,31 +33,30 @@ def task__arcadia__borrow(label: str, pool_address:str):
             for index, row in df.iterrows():
 
                 borrow_event = Borrow(
-                    pool_address = pool_address,
-                    account = "0x" + str(row["event__account"].hex()).lower(),
-                    by = "0x" + str(row["event__by"].hex()).lower(),
-                    to = "0x" + str(row["event__to"].hex()).lower(),
-                    amount =  str(row["event__amount_string"]),
-                    fee =  int(row["event__fee_string"]),
-                    referrer = "0x" + str(row["event__referrer"].hex()).lower(),
-
-                    transaction_hash = "0x" + str(row["transaction_hash"].hex()).lower(),
-                    transaction_index = row["transaction_index"],
-                    log_index = row["log_index"],
-                    chain = metadata.chain,
-                    block_number = row["block_number"]
+                    pool_address=pool_address,
+                    account=hex_cleaner(row["event__account"]),
+                    by=hex_cleaner(row["event__by"]),
+                    to=hex_cleaner(row["event__to"]),
+                    amount=str(row["event__amount_string"]),
+                    fee=int(row["event__fee_string"]),
+                    referrer=hex_cleaner(row["event__referrer"]),
+                    transaction_hash=hex_cleaner(row["transaction_hash"]),
+                    transaction_index=row["transaction_index"],
+                    log_index=row["log_index"],
+                    chain=metadata.chain,
+                    block_number=row["block_number"]
                 )
                 borrow_events.append(borrow_event)
-        
+
         Borrow.objects.bulk_create(borrow_events, ignore_conflicts=True)
         metadata.ingested = after_ingestion
         metadata.save()
-    
+
     update_timestamp(metadata.chain, Borrow.objects.filter(timestamp=None), Borrow)
 
 @shared_task(name="task__arcadia__auction_started_events")
-def task__arcadia__auction_started(label: str, pool_address:str):
-    
+def task__arcadia__auction_started(label: str, pool_address: str):
+
     metadata = CryoLogsMetadata.objects.get(label=label)
     files, after_ingestion = parquet_files_to_process(metadata.ingested, label)
 
@@ -69,26 +68,26 @@ def task__arcadia__auction_started(label: str, pool_address:str):
             for index, row in df.iterrows():
 
                 auction_started_event = AuctionStarted(
-                    pool_address = pool_address,
-                    account = "0x" + str(row["event__account"].hex()).lower(),
-                    creditor =  str(row["event__creditor"]),
-                    open_debt =  str(row["event__open_debt"]),
-
-                    transaction_hash = "0x" + str(row["transaction_hash"].hex()).lower(),
-                    transaction_index = row["transaction_index"],
-                    log_index = row["log_index"],
-                    chain = metadata.chain,
-                    block_number = row["block_number"]
+                    pool_address=pool_address,
+                    account=hex_cleaner(row["event__account"]),
+                    creditor=str(row["event__creditor"]),
+                    open_debt=str(row["event__open_debt"]),
+                    transaction_hash=hex_cleaner(row["transaction_hash"]),
+                    transaction_index=row["transaction_index"],
+                    log_index=row["log_index"],
+                    chain=metadata.chain,
+                    block_number=row["block_number"]
                 )
                 auction_started_events.append(auction_started_event)
-        
+
         AuctionStarted.objects.bulk_create(auction_started_events, ignore_conflicts=True)
         metadata.ingested = after_ingestion
         metadata.save()
+
     update_timestamp(metadata.chain, AuctionStarted.objects.filter(timestamp=None), AuctionStarted)
 
 @shared_task(name="task__arcadia__auction_finished_events")
-def task__arcadia__auction_finished(label: str, pool_address:str):
+def task__arcadia__auction_finished(label: str, pool_address: str):
     metadata = CryoLogsMetadata.objects.get(label=label)
     files, after_ingestion = parquet_files_to_process(metadata.ingested, label)
 
@@ -100,31 +99,31 @@ def task__arcadia__auction_finished(label: str, pool_address:str):
             for index, row in df.iterrows():
 
                 auction_finished_event = AuctionFinished(
-                    pool_address = pool_address,
-                    account = "0x" + str(row["event__account"].hex()).lower(),
-                    creditor =  str(row["event__creditor"]),
-                    start_debt =  str(row["event__start_debt"]),
-                    initiation_reward =  str(row["event__initiation_reward"]),
-                    termination_reward =  str(row["event__termination_reward"]),
-                    penalty =  str(row["event__penalty"]),
-                    bad_debt =  str(row["event__bad_debt"]),
-                    surplus =  str(row["event__surplus"]),
-                    
-                    transaction_hash = "0x" + str(row["transaction_hash"].hex()).lower(),
-                    transaction_index = row["transaction_index"],
-                    log_index = row["log_index"],
-                    chain = metadata.chain,
-                    block_number = row["block_number"]
+                    pool_address=pool_address,
+                    account=hex_cleaner(row["event__account"]),
+                    creditor=str(row["event__creditor"]),
+                    start_debt=str(row["event__start_debt"]),
+                    initiation_reward=str(row["event__initiation_reward"]),
+                    termination_reward=str(row["event__termination_reward"]),
+                    penalty=str(row["event__penalty"]),
+                    bad_debt=str(row["event__bad_debt"]),
+                    surplus=str(row["event__surplus"]),
+                    transaction_hash=hex_cleaner(row["transaction_hash"]),
+                    transaction_index=row["transaction_index"],
+                    log_index=row["log_index"],
+                    chain=metadata.chain,
+                    block_number=row["block_number"]
                 )
                 auction_finished_events.append(auction_finished_event)
-        
+
         AuctionFinished.objects.bulk_create(auction_finished_events, ignore_conflicts=True)
         metadata.ingested = after_ingestion
         metadata.save()
+
     update_timestamp(metadata.chain, AuctionFinished.objects.filter(timestamp=None), AuctionFinished)
 
 @shared_task(name="task__arcadia__repay_events")
-def task__arcadia__repay(label: str, pool_address:str):
+def task__arcadia__repay(label: str, pool_address: str):
     metadata = CryoLogsMetadata.objects.get(label=label)
     files, after_ingestion = parquet_files_to_process(metadata.ingested, label)
 
@@ -136,42 +135,23 @@ def task__arcadia__repay(label: str, pool_address:str):
             for index, row in df.iterrows():
 
                 repay_event = Repay(
-                    pool_address = pool_address,
-                    account = "0x" + str(row["event__account"].hex()).lower(),
-                    from_address =  str(row["event__from"]),
-                    amount =  str(row["event__amount"]),
-                    
-                    transaction_hash = "0x" + str(row["transaction_hash"].hex()).lower(),
-                    transaction_index = row["transaction_index"],
-                    log_index = row["log_index"],
-                    chain = metadata.chain,
-                    block_number = row["block_number"]
+                    pool_address=pool_address,
+                    account=hex_cleaner(row["event__account"]),
+                    from_address=str(row["event__from"]),
+                    amount=str(row["event__amount"]),
+                    transaction_hash=hex_cleaner(row["transaction_hash"]),
+                    transaction_index=row["transaction_index"],
+                    log_index=row["log_index"],
+                    chain=metadata.chain,
+                    block_number=row["block_number"]
                 )
                 repay_events.append(repay_event)
-        
+
         Repay.objects.bulk_create(repay_events, ignore_conflicts=True)
         metadata.ingested = after_ingestion
         metadata.save()
+
     update_timestamp(metadata.chain, Repay.objects.filter(timestamp=None), Repay)
-
-# @shared_task
-# def update_account_assets():
-    # accounts = Borrow.objects.values_list('account', flat=True).distinct()
-    # with transaction.atomic():
-        # for account in accounts:
-            # usdc_value = get_account_value(account, usdc_address)
-            # weth_value = get_account_value(account, weth_address)  # Adjust if needed
-            # asset_data = call_generate_asset_data(account)
-
-            # AccountAssets.objects.update_or_create(
-                # account=account,
-                # defaults={
-                    # 'usdc_value': str(usdc_value),
-                    # 'weth_value': str(weth_value),
-                    # 'asset_details': asset_data,
-                # }
-            # )
-
 
 @shared_task
 def task__arcadia__update_account_assets():
@@ -201,7 +181,6 @@ def task__arcadia__update_account_assets():
                 else:
                     update_amounts(account, asset_record)
 
-
 @shared_task
 def task__arcadia__metric_snapshot():
     # Annotate and cast debt and collateral for all accounts in a single query
@@ -209,7 +188,7 @@ def task__arcadia__metric_snapshot():
         debt_float=Cast('debt_usd', FloatField()),
         collateral_float=Cast('collateral_value_usd', FloatField())
     )
-    
+
     # Calculate total debt and collateral
     total_aggregates = annotated_assets.aggregate(
         total_debt=Sum('debt_float'),
