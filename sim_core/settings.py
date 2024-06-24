86748cef0bf454748014e9d0f5668aa2fe631029
rik61072@gmail.com <rik61072@gmail.com>
2024-06-24 21:16:37 +0530

    feat: sentry integration

--- sim_core/settings.py
+++ sim_core/settings.py
@@ -13,6 +13,7 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 from pathlib import Path
 import os
 from dotenv import load_dotenv
+import sentry_sdk
 load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -210,3 +211,16 @@ CACHES = {
         'LOCATION': 'unique-snowflake',
     }
 }
+
+if os.getenv("ENVIRONMENT", "").lower() == "production":
+    if os.getenv("SENTRY_DSN") is not None:
+        sentry_sdk.init(
+            dsn=os.getenv("SENTRY_DSN"),
+            # Set traces_sample_rate to 1.0 to capture 100%
+            # of transactions for performance monitoring.
+            traces_sample_rate=1.0,
+            # Set profiles_sample_rate to 1.0 to profile 100%
+            # of sampled transactions.
+            # We recommend adjusting this value in production.
+            profiles_sample_rate=1.0,
+        )
