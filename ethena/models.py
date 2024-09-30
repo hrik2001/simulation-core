04dd51e227f34db7216d6c575e8f86a5a4ec1227
rik61072@gmail.com <rik61072@gmail.com>
2024-09-30 23:58:42 +0530

    hot-fix: add index for ethena model

--- ethena/models.py
+++ ethena/models.py
@@ -40,6 +40,11 @@ class ChainMetrics(BaseModel):
     total_usdm_shares = models.TextField(null=False, default="0")
     total_superstate_ustb_balance = models.TextField(null=False, default="0")
 
+    class Meta(BaseModel.Meta):
+        indexes = [
+            models.Index(fields=['block_timestamp'], name='block_timestamp_idx'),
+        ]
+
 
 
 class CollateralMetrics(BaseModel):
