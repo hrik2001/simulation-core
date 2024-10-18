06d73d5cc1163695e96b4915c4561e75e5fc8138
FrancescoGenie <francesco.mangia@genieai.tech>
2024-10-18 09:44:42 +0200

    naming

--- curvesim/apps.py
+++ curvesim/apps.py
@@ -1,6 +1,6 @@
 from django.apps import AppConfig
 
 
-class DefimoneyConfig(AppConfig):
+class CurvesimConfig(AppConfig):
     default_auto_field = "django.db.models.BigAutoField"
     name = "curvesim"
