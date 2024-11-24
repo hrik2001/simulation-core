14e05379f0efc9b7625ac10cda587a8fd78fa0c5
ruler-of-hell <amCap1712@gmail.com>
2024-11-24 06:48:42 +0530

    register models and missing migrations

--- curve/admin.py
+++ curve/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
 
-# Register your models here.
+from curve.models import DebtCeiling, CurveMarketSnapshot, CurveMetrics, ControllerMetadata, \
+    CurveMarketSoftLiquidations, CurveMarketLosses, CurveMarkets, CurveLlammaEvents, CurveCr, CurveLlammaTrades
+
+admin.site.register(DebtCeiling)
+admin.site.register(ControllerMetadata)
+admin.site.register(CurveMetrics)
+admin.site.register(CurveMarketSnapshot)
+admin.site.register(CurveLlammaTrades)
+admin.site.register(CurveLlammaEvents)
+admin.site.register(CurveCr)
+admin.site.register(CurveMarkets)
+admin.site.register(CurveMarketSoftLiquidations)
+admin.site.register(CurveMarketLosses)
